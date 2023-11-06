f3c3bf8b0f2d15a6e70245f4c34c95025bfd8a45
Jefersonsteven <gfreycode@gmail.com>
2023-11-06 06:08:10 -0500

    Remove unused import and add DELETE route to delete list.

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,4 +1,4 @@
-import { createList, deleteList, getList } from "@/scripts/list";
+import { createList, deleteList } from "@/scripts/list";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -7,6 +7,18 @@ export async function POST(request: NextRequest) {
 
     try {
         const list = await createList(name, userId);
+        return NextResponse.json(list, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error Server",error }, { status: 500 });
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    const body = await request.json();
+    const { id } = body;
+
+    try {
+        const list = await deleteList(id);
         return NextResponse.json(list, { status: 201 });
     } catch (error) {
         return NextResponse.json({ message: "Error Server",error }, { status: 500 });

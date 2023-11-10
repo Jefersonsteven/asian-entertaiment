9fa76100c1e45fbce766da19736093bd6796f5d0
Jefersonsteven <gfreycode@gmail.com>
2023-11-10 11:48:58 -0500

    Remove ButtonLogin component and update global CSS styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import ButtonLogin from "./ui/ButtonLogin";
-
 export default function Home() {
 
-
   return (
-   <main className=" w-full h-full grid place-items-center bg-secondary-600 text-white">
-    <h1 className="animate-bounce">Hello world 🌎!!</h1>
-    <ButtonLogin />
+   <main>
    </main>
   )
 }
